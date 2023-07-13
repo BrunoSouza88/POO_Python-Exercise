b0c7d3996c510ec922ba307e107a82119d899015
BrunoSouza <desouza88@hotmail.com>
2023-07-13 20:53:51 -0300

    feat: exercise done

--- src/tv.py
+++ src/tv.py
@@ -1,22 +1,28 @@
 class TV:
     def __init__(self, tamanho: int):
-        pass
+        self.__tamanho = tamanho
+        self.__volume = 50
+        self.__canal = 1
+        self.__ligada = False
 
     def aumentar_volume(self):
-        pass
+        if self.__volume < 99:
+            self.__volume += 1
 
     def diminuir_volume(self):
-        pass
+        if self.__volume > 1:
+            self.__volume -= 1
 
     def modificar_canal(self, canal: int):
-        pass
+        if self.__canal < 1 and self.__canal > 99:
+            raise ValueError("Canal deve ser entre 1 e 99")
+        self.__canal = canal
 
     def ligar_desligar(self):
-        pass
+        self.__ligada = not self.__ligada
 
     def get_canal(self) -> int:
-        pass
+        return self.__canal
 
     def get_volume(self) -> int:
-        pass
-
+        return self.__volume
